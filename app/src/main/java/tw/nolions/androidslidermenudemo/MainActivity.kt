75216a71ec0aa21cd2c4039a17cc86e79c0f2801
NolionsChen <nolionschen@jetfuel.com.tw>
2020-08-07 15:31:16 +0800

    [feature] 右側滑入區塊

--- app/src/main/java/tw/nolions/androidslidermenudemo/MainActivity.kt
+++ app/src/main/java/tw/nolions/androidslidermenudemo/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.google.android.material.navigation.NavigationView
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.include_left_slide_menu.*
 
 
 class MainActivity : AppCompatActivity() {
